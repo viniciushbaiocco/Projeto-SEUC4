e17f5ca8fe37fc447bc155b49a77ec9e27dcfff0
Felipe Piva <felipe.ps5@puccampinas.edu.br>
2026-05-25 09:30:49 -0300

    Refactor SQL commands for consistency and clarity

--- comandos.sql
+++ comandos.sql
@@ -6,64 +6,96 @@ SELECT * FROM agendamentos;
 #1) COMANDOS TABELA AGENDAMENTOS
 
 #1-1) REMARCAR AGENDAMENTO
-#UPDATE agendamentos SET data_consulta = '2026-01-01 00:00:00' 
-#WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'EXEMPLO_NOME')
+#UPDATE agendamentos SET data_consulta = 'aaaa-mm-dd hh:mm:ss'
+#WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'EXEMPLO_NOME');
 
-UPDATE agendamentos SET data_consulta = '2026-05-22 16:30:00' 
-WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'Mariana Costa');
+UPDATE agendamentos SET data_consulta = '2026-05-22 16:30:00'
+WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'Mariana');
 
-#1-2) REMOVER AGENDAMENTO 
-#DELETE FROM agendamentos WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'EXEMPLO')
+#1-2) REMOVER AGENDAMENTO
+#DELETE FROM agendamentos
+#WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'EXEMPLO_NOME');
 
-DELETE FROM agendamentos WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'Carlos Eduardo');
+DELETE FROM agendamentos
+WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'Carlos');
 
-#1-3) ALTERAR DENTISTA DO AGENDAMENTO 
+#1-3) ALTERAR DENTISTA DO AGENDAMENTO
 #UPDATE agendamentos SET id_funcionario = (SELECT id_funcionario FROM funcionarios WHERE nome_funcionario = 'Nome do Novo Dentista')
 #WHERE id_funcionario = (SELECT id_funcionario FROM funcionarios WHERE nome_funcionario = 'Nome do Antigo Dentista')
-#AND data_consulta = '2026-01-01 00:00:00';
+#AND data_consulta = 'aaaa-mm-dd hh:mm:ss';
 
-UPDATE agendamentos SET id_funcionario = (SELECT id_funcionario FROM funcionarios WHERE nome_funcionario = 'Joyce Professora Incrivel') 
-WHERE id_funcionario = (SELECT id_funcionario FROM funcionarios WHERE nome_funcionario = 'Neymar Junior') 
+UPDATE agendamentos SET id_funcionario = (SELECT id_funcionario FROM funcionarios WHERE nome_funcionario = 'Joyce')
+WHERE id_funcionario = (SELECT id_funcionario FROM funcionarios WHERE nome_funcionario = 'Neymar')
 AND data_consulta = '2026-05-22 14:00:00';
 
-#1-4) ALTERAR VALOR DO AGENDAMNETO 
-#UPDATE agendamentos SET valor = 999 
-#WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'Nome Cliente') 
-#AND data_consulta = '2026-01-01 00:00:00';
+#1-4) ALTERAR VALOR DO AGENDAMENTO
+#UPDATE agendamentos SET valor = 999
+#WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'EXEMPLO_NOME')
+#AND data_consulta = 'aaaa-mm-dd hh:mm:ss';
 
 UPDATE agendamentos SET valor = 150
-WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'Mariana Costa')
+WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'Mariana')
 AND data_consulta = '2026-05-22 16:30:00';
-#COLOCAR O CASO DO CONVENIO
+
+#1-5) REGISTRAR PAGAMENTO DO AGENDAMENTO
+#UPDATE agendamentos SET pago = TRUE
+#WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'EXEMPLO_NOME')
+#AND data_consulta = 'aaaa-mm-dd hh:mm:ss';
+
+UPDATE agendamentos SET pago = TRUE
+WHERE id_cliente = (SELECT id_cliente FROM clientes WHERE nome_cliente = 'Mariana')
+AND data_consulta = '2026-05-22 16:30:00';
 
 
 #2) COMANDOS TABELA CLIENTES
 
 #2-1) ALTERAR CELULAR CLIENTE
-#UPDATE clientes SET celular_cliente = '11999999999' WHERE nome_cliente = 'Nome Cliente';
+#UPDATE clientes SET celular_cliente = '11999999999'
+#WHERE nome_cliente = 'EXEMPLO_NOME';
 
-UPDATE clientes SET celular_cliente = '19996863809' WHERE nome_cliente = 'Ana Silva';
+UPDATE clientes SET celular_cliente = '19996863809'
+WHERE nome_cliente = 'Ana';
 
 #2-2) ALTERAR CONVÊNIO CLIENTE
-#UPDATE clientes SET convenio = 0/1 WHERE nome_cliente = 'Nome Cliente';
+#UPDATE clientes SET id_convenio = id_do_convenio
+#WHERE nome_cliente = 'EXEMPLO_NOME';
 
-UPDATE clientes SET convenio = 1 WHERE nome_cliente = 'João Pedro';
+UPDATE clientes SET id_convenio = 2
+WHERE nome_cliente = 'João';
+
+#2-3) ALTERAR NOME SOCIAL DO CLIENTE
+#UPDATE clientes SET nome_social = 'Nome Social'
+#WHERE nome_cliente = 'EXEMPLO_NOME';
+
+UPDATE clientes SET nome_social = 'Exemplo Nome Social'
+WHERE nome_cliente = 'Ana';
+
+#2-4) REMOVER NOME SOCIAL DO CLIENTE
+#UPDATE clientes SET nome_social = NULL
+#WHERE nome_cliente = 'EXEMPLO_NOME';
+
+UPDATE clientes SET nome_social = NULL
+WHERE nome_cliente = 'Ana';
 
 
 #3) COMANDOS TABELA FUNCIONÁRIOS
 
 #3-1) ALTERAR CELULAR FUNCIONÁRIO
-#UPDATE funcionarios SET celular_funcionario = '11999999999' WHERE nome_funcionario = 'Nome Funcionário';
+#UPDATE funcionarios SET celular_funcionario = '11999999999'
+#WHERE nome_funcionario = 'EXEMPLO_NOME';
 
-UPDATE funcionarios SET celular_funcionario = '19996864069' WHERE nome_funcionario = 'Fabiana Melo';
+UPDATE funcionarios SET celular_funcionario = '19996864069'
+WHERE nome_funcionario = 'Fabiana';
 
-#3-2)
-#UPDATE funcionarios SET salario = 9999 WHERE nome_funcionario = 'Nome Funcionário';
+#3-2) ALTERAR SALÁRIO FUNCIONÁRIO
+#UPDATE funcionarios SET salario = 9999
+#WHERE nome_funcionario = 'EXEMPLO_NOME';
 
-UPDATE funcionarios SET salario = 2600 WHERE nome_funcionario = 'Luis Gustavo Costa';
+UPDATE funcionarios SET salario = 2600
+WHERE nome_funcionario = 'Luis Gustavo';
 
-#4) TABELAS APÓS EDIÇÃO 
 
+#4) TABELAS APÓS EDIÇÃO
 SELECT * FROM agendamentos;
 SELECT * FROM funcionarios;
 SELECT * FROM clientes;
