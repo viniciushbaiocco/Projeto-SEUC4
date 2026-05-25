d559df661a1cbb376f25ae57e97fa48f859353ec
Felipe Piva <felipe.ps5@puccampinas.edu.br>
2026-05-25 09:29:59 -0300

    Refactor database schema and add convenios table
    Updated table definitions for funcionarios, clientes, and agendamentos. Added convenios table and modified existing columns.

--- criação database e tabelas.sql
+++ criação database e tabelas.sql
@@ -3,30 +3,40 @@ USE odonpuc;
 
 CREATE TABLE funcionarios (
 id_funcionario INT PRIMARY KEY AUTO_INCREMENT,
-nome_funcionario VARCHAR(100) NOT NULL,
-email VARCHAR(100) UNIQUE, 
+nome_funcionario VARCHAR(50) NOT NULL,
+sobrenome_funcionario VARCHAR(50) NOT NULL,
+email VARCHAR(100) UNIQUE,
 celular_funcionario CHAR(11) UNIQUE NOT NULL,
 cargo VARCHAR(50) NOT NULL,
 salario INT NOT NULL);
 
+CREATE TABLE convenios (
+id_convenio INT PRIMARY KEY AUTO_INCREMENT,
+nome_convenio VARCHAR(100) DEFAULT NULL
+);
+
 CREATE TABLE clientes (
 id_cliente INT PRIMARY KEY AUTO_INCREMENT,
-nome_cliente VARCHAR(100) NOT NULL,
+nome_cliente VARCHAR(50) NOT NULL,
+sobrenome_cliente VARCHAR(50),
+nome_social VARCHAR(50),
 idade CHAR(3) NOT NULL,
 celular_cliente CHAR(11) UNIQUE NOT NULL,
-convenio BOOLEAN,
-cpf CHAR(11) UNIQUE NOT NULL);
+cpf CHAR(11) UNIQUE NOT NULL,
+id_convenio INT NOT NULL,
+data_criacao_usuario DATETIME DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (id_convenio) REFERENCES convenios(id_convenio)
+);
 
 CREATE TABLE agendamentos (
 id_agendamento INT PRIMARY KEY AUTO_INCREMENT,
-id_funcionario INT,
-id_cliente INT,
-data_consulta DATETIME,
+id_funcionario INT NOT NULL,
+id_cliente INT NOT NULL,
+data_consulta DATETIME NOT NULL,
 valor INT DEFAULT 00,
+pago BOOLEAN DEFAULT FALSE,
 FOREIGN KEY (id_funcionario) REFERENCES funcionarios(id_funcionario),
 FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente));
 
-
-
-
+#auditoria - dia que gera pagamento registrado e criação dos clientes e funcionarios (pesquisa)
 
